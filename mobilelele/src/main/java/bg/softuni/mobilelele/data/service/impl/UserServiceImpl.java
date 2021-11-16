f4a258a4834f9931707a4fbdc0781f4a1ca9c51f
Arcerr <taurus..ali47>
2021-11-16 22:44:17 +0200

    Registration with Spring security

--- mobilelele/src/main/java/bg/softuni/mobilelele/data/service/impl/UserServiceImpl.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/data/service/impl/UserServiceImpl.java
@@ -3,16 +3,18 @@ package bg.softuni.mobilelele.data.service.impl;
 import bg.softuni.mobilelele.data.model.entity.enums.UserRoleEnum;
 import bg.softuni.mobilelele.data.model.entity.UserEntity;
 import bg.softuni.mobilelele.data.model.entity.UserRoleEntity;
-import bg.softuni.mobilelele.data.model.service.UserLoginServiceModel;
 import bg.softuni.mobilelele.data.model.service.UserRegistrationServiceModel;
 import bg.softuni.mobilelele.data.repository.UserRepository;
 import bg.softuni.mobilelele.data.repository.UserRoleRepository;
 import bg.softuni.mobilelele.data.service.UserService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,11 +22,13 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
+    private final MobileleUserServiceImpl mobileleUserService;
 
-    public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, UserRoleRepository userRoleRepository) {
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, UserRoleRepository userRoleRepository, MobileleUserServiceImpl mobileleUserService) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.userRoleRepository = userRoleRepository;
+        this.mobileleUserService = mobileleUserService;
     }
 
     @Override
@@ -49,12 +53,22 @@ public class UserServiceImpl implements UserService {
 
         userRepository.save(newUser);
 
+        // this is the Spring representation of a user
+        UserDetails userDetails = mobileleUserService.loadUserByUsername(newUser.getUsername());
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                newUser.getPassword(),
+                userDetails.getAuthorities()
+        );
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authentication);
         //TODO: register user
     }
 
     @Override
     public boolean isUsernameFree(String username) {
-        return userRepository.findByUsername(username) == null;
+        return userRepository.findByUsername(username).isEmpty();
     }
 
 
